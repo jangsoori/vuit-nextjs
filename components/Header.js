9050db36f5d001ddcdf6a52bab9520630166c355
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-12-10 15:43:51 +0000

    Added Header - Logo, Searchbar

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+import React from "react";
+import Search from "./Search";
+const StyledHeader = styled.header`
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 3rem;
+`;
+const Logo = styled.h1`
+  font-size: 2.4rem;
+  color: white;
+  cursor: pointer;
+`;
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <Logo>vu.it</Logo>
+      <Search />
+    </StyledHeader>
+  );
+}
