5ed0fce29093293b13b7bac6064632fa23172c6d
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-12-10 15:46:37 +0000

    Added GlobalReset, Theme, Cache, Context Providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import GlobalReset from "../styles/GlobalReset";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "../styles/theme";
+import { ReactQueryCacheProvider, QueryCache } from "react-query";
+import SearchProvider from "../context/searchContext";
+import { Hydrate } from "react-query/hydration";
 
+const queryCache = new QueryCache({
+  defaultConfig: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+import styled from "@emotion/styled";
+import QueryProvider from "../context/queryContext";
+const StyledApp = styled.div`
+  font-family: "Quicksand";
+  background: ${({ theme }) => theme.colors.primary};
+  height: 100%;
+
+  padding: 2rem;
+`;
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalReset />
+      <ThemeProvider theme={theme}>
+        <StyledApp>
+          <ReactQueryCacheProvider queryCache={queryCache}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <SearchProvider>
+                <QueryProvider>
+                  <Component {...pageProps} />
+                </QueryProvider>
+              </SearchProvider>
+            </Hydrate>
+          </ReactQueryCacheProvider>
+        </StyledApp>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
