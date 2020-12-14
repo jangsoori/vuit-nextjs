693103fd433061cfa8d16a45d38b4cefe8bc504c
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-12-14 09:14:18 +0000

    Added subreddit / multireddit name

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SortFilters from "./SortFilters";
 import Posts from "./Posts";
 import Loading from "./Loader";
 import PaginationBtns from "./PaginationBtns";
+import Loader from "react-spinners/ClipLoader";
 const MainWrapper = styled.section`
   /* height: 200vh; */
 `;
@@ -18,17 +19,43 @@ const Subreddit = styled.p`
   font-size: 2rem;
   font-weight: 500;
   margin-right: 1rem;
+  position: relative;
+  font-family: "Quicksand";
+  font-weight: bold;
 `;
 export default function Main({ data, isLoading, isFetching }) {
-  //Fetch data
+  const [subreddits, setSubreddits] = useState([]);
+  useEffect(() => {
+    const subNames = [
+      ...new Set(
+        data?.data?.data?.children.map((child) => {
+          return child.data.subreddit;
+        })
+      ),
+    ];
+    setSubreddits(subNames);
+    return () => {
+      setSubreddits([]);
+    };
+  }, [data]);
+
+  const renderSubbreditNames = () => {
+    if (subreddits.length === 1) {
+      return subreddits.map((subreddit) => {
+        return <span>r/{subreddit}</span>;
+      });
+    } else if (subreddits.length > 1) {
+      return <span>Multireddit</span>;
+    }
+  };
   return (
     <MainWrapper>
       <Header>
-        <Subreddit>r/placeholder</Subreddit>
+        <Subreddit>{subreddits && renderSubbreditNames()}</Subreddit>
         <SortFilters />
       </Header>
       {isFetching ? (
-        <Loading />
+        <Loading size={50} />
       ) : (
         <>
           <Posts
